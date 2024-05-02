8619760c93c678403424f3ba4fe2f62149445d0e
hunter.ladd <dhunter.ladd@gmail.com>
2024-05-02 15:40:10 -0700

    Convert detail to comment, and change label

--- test/automated/insert_line_numbers.rb
+++ test/automated/insert_line_numbers.rb
@@ -2,10 +2,10 @@ require_relative 'automated_init'
 
 context "Insert Line Numbers" do
   text = Controls::Text.example
-  detail "Text:", text
+  comment "Text:", text
 
   numbered_text = CompareText::InsertLineNumbers.(text)
-  comment "Numbered Text:", numbered_text
+  comment "Line Numbers Inserted:", numbered_text
 
   control_text = Controls::Text::Numbered.example
   detail "Control:", control_text
