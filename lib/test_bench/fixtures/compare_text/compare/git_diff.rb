a3de0d1a3451a8a17bf0c21f66758b245670ede4
hunter.ladd <dhunter.ladd@gmail.com>
2024-06-16 10:14:44 -0700

    GitDiff class invokes git diff command

--- lib/test_bench/fixtures/compare_text/compare/git_diff.rb
+++ lib/test_bench/fixtures/compare_text/compare/git_diff.rb
@@ -8,12 +8,17 @@ module TestBench
           initializer :text, :different_text
 
           def call
-            control_text_path = write_tempfile(text, 'control')
-            compare_text_path = write_tempfile(different_text, 'compare')
+            control_path = write_tempfile(text, 'control')
+            compare_path = write_tempfile(different_text, 'compare')
 
-            puts control_text_path.inspect
-            puts compare_text_path.inspect
-            ''
+            ## Replace static value 111 with value appropriate for input texts
+            diff_command = "git diff --unified=111 --color --word-diff #{control_path} #{compare_path}"
+
+            diff_output = run_command(diff_command)
+
+            formatted_diff = format_diff(diff_output)
+
+            formatted_diff
           end
 
           def write_tempfile(text, prefix)
@@ -24,6 +29,24 @@ module TestBench
             tempfile.close
 
             tempfile.path
+          end
+
+          def run_command(command)
+            `#{command}`
+          end
+
+          def format_diff(diff_output)
+            raw_lines = diff_output.each_line.to_a
+
+            lines = raw_lines.drop_while do |line|
+              line.gsub!(/\e\[(\d+)m/, '')
+
+              !line.start_with?('@@')
+            end
+
+            lines.delete_at(0)
+
+            lines.join
           end
         end
       end
